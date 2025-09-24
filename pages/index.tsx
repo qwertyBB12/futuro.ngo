7235c14de921cfc0a1dafe785f9e6a4717dff606
qwertyBB12 <144513833+qwertyBB12@users.noreply.github.com>
2025-09-24 06:29:23 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,3 @@
-// pages/index.tsx
-
-import * as React from "react";
-import Head from "next/head";
-import { useRouter } from "next/router";
-
-// Plasmic imports
-import { PlasmicHomepage } from "../plasmic/futuro_ngo/PlasmicHomepage";
-import { GlobalContextsProvider } from "../plasmic/futuro_ngo/PlasmicGlobalContexts";
-import { PageParamsProvider__ } from "../plasmic/futuro_ngo/PlasmicUtils";
-
 export default function Homepage() {
   const router = useRouter();
 
@@ -21,7 +10,6 @@ export default function Homepage() {
             name="description"
             content="A platform uniting leaders, institutions, and projects across the Americas to design, fund, and scale social impact."
           />
-
           {/* Open Graph */}
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://futuro.ngo/" />
@@ -33,7 +21,10 @@ export default function Homepage() {
             property="og:description"
             content="A platform uniting leaders, institutions, and projects across the Americas to design, fund, and scale social impact."
           />
-          <meta property="og:image" content="https://futuro.ngo/OG_Futuro.png" />
+          <meta
+            property="og:image"
+            content="https://futuro.ngo/OG_Futuro.png"
+          />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
 
