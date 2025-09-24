cd99fa921adfe68249790b16504204305ad4064a
qwertyBB12 <144513833+qwertyBB12@users.noreply.github.com>
2025-09-23 20:59:50 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -21,13 +21,34 @@ export default function Document() {
 
         {/* Theme color for mobile browsers */}
         <meta name="theme-color" content="#1B2A41" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-<link rel="canonical" href="https://futuro.ngo/" />
-<meta property="og:site_name" content="Futuro" />
-<meta name="twitter:card" content="summary_large_image" />
-<link rel="preconnect" href="https://fast.wistia.com" />
-<link rel="preconnect" href="https://embedwistia-a.akamaihd.net" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="canonical" href="https://futuro.ngo/" />
+        <meta property="og:site_name" content="Futuro" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="preconnect" href="https://fast.wistia.com" />
+        <link rel="preconnect" href="https://embedwistia-a.akamaihd.net" />
+    
+    <script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Futuro",
+  "url": "https://futuro.ngo",
+  "logo": "https://futuro.ngo/favicon-512x512.png",
+  "sameAs": ["https://www.linkedin.com/", "https://x.com/"] 
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "Futuro",
+  "url": "https://futuro.ngo",
+  "potentialAction":
+  }
+}
+</script>
       </Head>
       <body>
         <Main />
